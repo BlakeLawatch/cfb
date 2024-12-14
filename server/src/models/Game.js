78809d24f57bea0539647c8cd3f46c35f4935435
Blake Lawatch <blakelawatch@gmail.com>
2024-12-13 21:39:02 -0700

    game model

--- server/src/models/Game.js
+++ server/src/models/Game.js
@@ -1,11 +1,16 @@
+import { json } from "express/lib/response";
 import { Schema } from "mongoose";
 
 export const GameSchema = new Schema(
     {
         matchId: { type: Schema.Types.ObjectId },
-        team1: { type: String, required: true },
-        team2: { type: String, required: true },
-        points: { type: String, required: true },
+        team1Id: { type: Schema.Types.ObjectId },
+        team2Id: { type: Schema.Types.ObjectId },
+        points: { type: Number, required: true },
         winnerId: { type: Schema.Types.ObjectId }
-    }
-)
+    },
+    { timestamps: true, toJSON: { virtuals: true } })
+
+GameSchema.virtual('creator', {
+    localField: 
+    })
